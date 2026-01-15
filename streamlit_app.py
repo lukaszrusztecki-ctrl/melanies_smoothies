a95259038c4d1991306b436ca689470e0b1017a1
lukaszrusztecki-ctrl <lukasz.rusztecki@gmail.com>
2026-01-16 00:18:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,8 @@ if ingredients_list:
             icon="✅"
         )
 
-     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon"
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon"
         )
-     st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True
+    st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True
         )
 
